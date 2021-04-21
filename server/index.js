29fd5717003c47a145c201cdc80ba42843aa5b96
D Varun Venkat Reddy <43230571+VarunTechguy@users.noreply.github.com>
2021-04-21 10:21:12 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+dotenv.config();
+//Every routes in postroutes starts with /posts
+
+
+//General setup
+app.use(bodyParser.json({limit:"30mb", extend:true}))
+app.use(bodyParser.urlencoded({limit:"30mb", extend:true}))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+// app.get('/', (req,res) =>{
+//     res.send('Hello to blog api');
+// });
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology:true})
+ .then(() => app.listen(PORT, () => console.log('Server Running on port:', PORT)))
+ .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+//using cloud atlast version 
